680580b24019980e91bcd356fd4fcc4743eeb226
Mishar025 <mishamay583@gmail.com>
2024-06-09 19:42:08 -0400

    сff

---
+++ services/config-server2/src/test/java/com/mishka/config_server2/ConfigServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mishka.config_server2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
