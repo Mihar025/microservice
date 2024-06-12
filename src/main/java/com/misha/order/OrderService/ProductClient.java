c809a6d13c5986ea77f8aea3dbf4b1ba127dfd95
Mishar025 <mishamay583@gmail.com>
2024-06-12 15:54:43 -0400

    сf

--- src/main/java/com/misha/order/OrderService/ProductClient.java
+++ src/main/java/com/misha/order/OrderService/ProductClient.java
@@ -4,10 +4,18 @@ import com.misha.order.Controller.PurchaseRequest;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
 import java.util.List;
+
+import static org.springframework.http.HttpMethod.POST;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +25,18 @@ public class ProductClient {
     private final RestTemplate restTemplate;
 
     public List<PurchaseResponse> purchaseProducts(List<PurchaseRequest> requestBody){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
+        HttpEntity<List<PurchaseRequest>> requestEntity = new HttpEntity<>(requestBody, httpHeaders);
+        ParameterizedTypeReference<List<PurchaseResponse>> responseType = new ParameterizedTypeReference<List<PurchaseResponse>>() {};
+        ResponseEntity<List<PurchaseResponse>> responseEntity = restTemplate.exchange(
+                productUrl + "/purchase",
+                POST,
+                requestEntity,
+                responseType
+        );
+        return responseEntity.getBody();
     }
 
 
