e239c0dabebb4e13b812d117b18b5ace6ce39941
Mishar025 <mishamay583@gmail.com>
2024-06-12 15:36:16 -0400

    сf

--- src/main/java/com/misha/order/OrderService/PurchaseResponse.java
+++ src/main/java/com/misha/order/OrderService/PurchaseResponse.java
@@ -1,7 +1,14 @@
 package com.misha.order.OrderService;
 
+import java.math.BigDecimal;
+
 public record PurchaseResponse(
 
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
 
 
 
